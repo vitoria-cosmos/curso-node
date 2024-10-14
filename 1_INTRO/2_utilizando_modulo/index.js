7d8317ca44fe64428ef320b1af097f1e34b0e3c0
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-10-14 13:08:41 -0300

    first commit

---
+++ 1_INTRO/2_utilizando_modulo/index.js
@@ -0,0 +1,23 @@
+
+// importando o módulo fire system
+const fs = require('fs');
+
+// usar um método
+// o fs é como classe, objeto, ele tem métodos que podemos usar
+// para trabalhar com arquivos e diretórios
+
+fs.readFile('arquivo.txt', 'utf8', (err, data) => {
+
+    // se der algum erro, ele vai imprimir o erro, se não, ele vai imprimir os dados
+
+    if(err) {
+        console.log(err);
+        return;
+    }
+
+    console.log(data);
+    // aqui estamos imprimindo na tela o conteúdo do arquivo txt
+} );
+// primeiro parametro é o caminho para o arquivo
+// o outro parametro é o incode do arquivo
+// o otro parametro é uma função anonime em que posso receber um erro ou os dados
---
+++ exercicios/1_tarefa/programa.js
@@ -0,0 +1,5 @@
+
+const num1 = 10;
+const num2 = 2;
+
+console.log('A soma entre as variáveis é igual a: ', num1 + num2);
