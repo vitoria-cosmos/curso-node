d79d32124d929865886871f43869b3da2c48c02c
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-10-18 17:59:23 -0300

    aprendendo sobre argumentos na linha de comando

---
+++ 2_FUNDAMENTOS/4_ler_argumentos/index.js
@@ -0,0 +1,29 @@
+
+
+// nome
+
+console.log(process.argv)
+
+// pegar os nossos argumentos
+
+const args = process.argv.slice(2)
+// o slice resgata o segundo indice da lista
+console.log(args)
+
+//pegar só o nome
+// vou dividir a string quando aparecer um =
+const nome = args[0].split('=')[1]
+console.log(nome)
+
+// resgata o segundo argumento
+const idade = args[1].split('=')[1]
+console.log(idade)
+
+// imprimir o nome e a idade
+console.log(`O nome dela é ${nome} e ela tem ${idade} anos!`);
+
+// quero agora pegar o sexo
+const sexo = args[2].split('=')[1];
+console.log(sexo);
+
+
