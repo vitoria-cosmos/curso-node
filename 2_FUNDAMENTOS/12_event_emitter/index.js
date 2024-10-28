39d724517339fe8e05c62339b6d85c51df4482d9
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-10-28 14:08:46 -0300

    aprendendo sobre Event Emitter

---
+++ 2_FUNDAMENTOS/12_event_emitter/index.js
@@ -0,0 +1,21 @@
+
+// vamos chamar o modulo EventEmmiter
+const EventEmitter = require('events')
+
+// variavel com e minusculo(covencao) vai instanciar a classe
+const eventEmitter = new EventEmitter()
+
+// Define o evento
+// evento global
+
+eventEmitter.on('start', () => {
+    console.log('Durante')
+})
+
+// inicio
+console.log('Antes')
+
+// ativar, emitir o evento
+eventEmitter.emit('start')
+
+console.log('Depois')
